71ae00b822898ff28a0631e1a4917e8683484086
Matthew Hartley <mhartley@cantab.net>
2019-11-21 08:38:27 +0000

    Add README writing to conversion script

--- scripts/convert_mnist_data.py
+++ scripts/convert_mnist_data.py
@@ -11,10 +11,26 @@ from sklearn.datasets import fetch_mldata
 from dtool_utils.quick_dataset import QuickDataSet
 
 
+README_TEMPLATE = """---
+dataset_name: MNIST handwritten digits
+project: dtoolAI demonstration datasets
+authors:
+  - Yann LeCun
+  - Corinna Cortes
+  - Christopher J.C. Burges
+origin: http://yann.lecun.com/exdb/mnist/
+"""
+
+
 def get_mnist_from_sklearn():
     mnist = fetch_mldata("MNIST original")
 
-    with QuickDataSet('scratch', 'mnist') as qds:
+    return mnist
+
+
+def create_dataset_from_mnist_data(mnist, output_base_uri, output_name):
+
+    with QuickDataSet(output_base_uri, output_name) as qds:
         numpy_fpath = qds.staging_fpath('mnist.npy')
         np.save(numpy_fpath, mnist.data)
         labels_fpath = qds.staging_fpath('labels.npy')
@@ -26,10 +42,16 @@ def get_mnist_from_sklearn():
     image_dim = [1, 28, 28]
     qds.put_annotation("image_dimensions", image_dim)
 
+    qds.put_readme(README_TEMPLATE)
 
-def main():
+
+@click.command()
+@click.argument('output_base_uri')
+@click.argument('output_name')
+def main(output_base_uri, output_name):
     
-    get_mnist_from_sklearn()
+    mnist = get_mnist_from_sklearn()
+    create_dataset_from_mnist_data(mnist, output_base_uri, output_name)
 
 
 if __name__ == "__main__":
