5f5db0999cda9e56b6cd529f02b27cbd34dd80cb
Matthew Hartley <mhartley@cantab.net>
2020-01-06 08:11:23 +0000

    Add README writing to CIFAR10 data prep

--- scripts/dataprep/convert_cifar10_data.py
+++ scripts/dataprep/convert_cifar10_data.py
@@ -7,9 +7,22 @@ import dtoolcore
 
 import numpy as np
 
-# from imageio import imsave
-
 from dtool_utils.quick_dataset import QuickDataSet
+
+
+README_TEMPLATE = """---
+dataset_name: CIFAR-10 (10 category subset of 80 million tiny images)
+project: dtoolAI demonstration datasets
+authors:
+  - Alex Krizhevsky
+  - Vinod Nair
+  - Geoffrey Hinton
+origin: https://www.cs.toronto.edu/~kriz/cifar.html
+reference: |
+  Learning Multiple Layers of Features from Tiny Images 
+  <https://www.cs.toronto.edu/~kriz/learning-features-2009-TR.pdf>
+usetype: {usetype}
+"""
 
 
 def data_and_labels_from_fname_list(cifar_dirpath, fname_list):
@@ -29,11 +42,14 @@ def data_and_labels_from_fname_list(cifar_dirpath, fname_list):
     return data, labels
 
 
-def convert_cifar10_data(cifar_dirpath, output_base_uri, output_name):
+def convert_cifar10_data(cifar_dirpath, output_base_uri, output_name, mode='train'):
 
-    # batches = [f'data_batch_{n}' for n in range(1, 6)]
-    test_batch = ['test_batch']
-    data, labels = data_and_labels_from_fname_list(cifar_dirpath, test_batch)
+    if mode == 'train':
+        batches = [f'data_batch_{n}' for n in range(1, 6)]
+    elif mode == 'test':
+        batches = ['test_batch']
+
+    data, labels = data_and_labels_from_fname_list(cifar_dirpath, batches)
 
     with QuickDataSet(output_base_uri, output_name) as qds:
         data_abspath = qds.staging_fpath('cifar.npy')
@@ -46,40 +62,20 @@ def convert_cifar10_data(cifar_dirpath, output_base_uri, output_name):
     
     image_dim = [3, 32, 32]
     qds.put_annotation("image_dimensions", image_dim)
+    qds.put_annotation("dtoolAI.inputtype", "TensorDataSet")
+    qds.put_readme(README_TEMPLATE.format(usetype=mode))
+
 
 
 @click.command()
 @click.argument('cifar_base_dirpath')
 @click.argument('output_base_uri')
 @click.argument('output_name')
-def main(cifar_base_dirpath, output_base_uri, output_name):
+@click.option('--mode', default='train')
+def main(cifar_base_dirpath, output_base_uri, output_name, mode):
     
     cifar_dirpath = Path(cifar_base_dirpath)
-    convert_cifar10_data(cifar_dirpath, output_base_uri, output_name)
-    # cifar_dirpath = Path(cifar_base_dirpath)
-
-    # with open(cifar_dirpath/'data_batch_1', 'rb') as fh:
-    #     d = pickle.load(fh, encoding='bytes')
-
-    # data = d[b'data']
-
-    # first = data[1]
-
-    # reshaped = first.reshape((3, 32, 32))
-    # transposed = reshaped.transpose((1, 2, 0))
-
-    # print(transposed.shape)
-
-    # imsave('test.png', transposed)
-
-    # ln = d[b'labels'][1]
-
-    # with open(cifar_dirpath/'batches.meta', 'rb') as fh:
-    #     d = pickle.load(fh, encoding='bytes')
-
-    # print(d[b'label_names'][ln])
-
-    # first.reshape(3, 32, 32)
+    convert_cifar10_data(cifar_dirpath, output_base_uri, output_name, mode)
 
 
 if __name__ == "__main__":
