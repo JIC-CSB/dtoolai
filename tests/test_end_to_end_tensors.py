4ff6e2f6bdd8ed411590d43d65db24b5b8523a43
Matthew Hartley <mhartley@cantab.net>
2020-01-24 14:54:00 +0000

    Add end to end test for MNIST tensor training

---
+++ tests/test_end_to_end_tensors.py
@@ -0,0 +1,70 @@
+import os
+
+import pytest
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from . import tmp_dir_fixture
+from . import TEST_SAMPLE_DATA
+
+
+@pytest.mark.slow
+def test_end_to_end_tensors_with_mnist(tmp_dir_fixture):
+
+    mnist_train_numpy_dirname = "mnist_train_numpy"
+    data_fpath = os.path.join(TEST_SAMPLE_DATA, mnist_train_numpy_dirname, "data.npy")
+    labels_fpath = os.path.join(TEST_SAMPLE_DATA, mnist_train_numpy_dirname, "labels.npy")
+
+    data = np.load(data_fpath)
+    assert data.shape == (60000, 784)
+    labels = np.load(labels_fpath)
+    assert labels.shape == (60000,)
+
+    from dtoolai.data import create_tensor_dataset_from_arrays
+
+    create_tensor_dataset_from_arrays(
+        tmp_dir_fixture,
+        "mnist.train",
+        data,
+        labels,
+        (1, 28, 28),
+        ""
+    )
+
+    from dtoolai.data import TensorDataSet
+
+    mnist_train_uri = os.path.join(tmp_dir_fixture, "mnist.train")
+    tds = TensorDataSet(mnist_train_uri)
+    assert len(tds) == 60000
+
+    dl = DataLoader(tds, batch_size=128, shuffle=True)
+    data, labels = next(iter(dl))
+    assert data.shape == (128, 1, 28, 28)
+
+    from dtoolai.models import GenNet
+    init_params = dict(input_channels=tds.input_channels, input_dim=tds.dim)
+    model = GenNet(**init_params)
+    loss_fn = F.nll_loss
+    optimiser = optim.SGD(model.parameters(), lr=0.01)
+
+    from dtoolai.utils import train
+    train(model, dl, optimiser, loss_fn, 2)
+
+    # FIXME - should be hosted or something
+    model.eval()
+    tds_test = TensorDataSet("scratch/datasets/mnist.test")
+    dl_test = DataLoader(tds_test, batch_size=128)
+    assert len(tds_test) == 10000
+    correct = 0
+    with torch.no_grad():
+        for data, label in dl_test:
+            Y_pred = model(data)
+            pred = Y_pred.argmax(dim=1, keepdim=True)
+            correct += pred.eq(label.view_as(pred)).sum().item()
+
+    assert correct >= 6000
+
+
