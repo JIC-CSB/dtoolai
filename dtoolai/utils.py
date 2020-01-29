6f721319af812dac996b306f07299eebd59070e3
Matthew Hartley <mhartley@cantab.net>
2020-01-29 15:46:26 +0000

    Move useful functions to utils

--- dtoolai/utils.py
+++ dtoolai/utils.py
@@ -10,6 +10,32 @@ import numpy as np
 from dtoolai.trained import TrainedTorchModel
 
 
+def coerce_to_target_dim(im, input_format):
+    """Convert a PIL image to a fixed size and number of channels."""
+
+    mode_map = {
+        1: 'L',
+        3: 'RGB',
+        4: 'RGBA'
+    }
+
+    if im.mode not in mode_map.values():
+        raise Exception(f"Unknown image mode: {im.mode}")
+
+    ch, tdimw, tdimh = input_format
+    if ch not in mode_map:
+        raise Exception(f"Unsupported input format: {input_format}")
+
+    cdimw, cdimh = im.size
+    if (cdimw, cdimh) != (tdimw, tdimh):
+        im = im.resize((tdimw, tdimh))
+
+    if mode_map[ch] != im.mode:
+        im = im.convert(mode_map[ch])
+
+    return im
+    
+
 @click.command()
 @click.argument('model_uri')
 def print_provenance(model_uri):
--- scripts/apply_model_to_image.py
+++ scripts/apply_model_to_image.py
@@ -1,46 +1,12 @@
 import click
-import torch
 import dtoolcore
-
-import numpy as np
 
 from PIL import Image
 
-import torch.nn as nn
-import torchvision.models
 from torchvision.transforms.functional import to_tensor
 
-from dtoolai.models import GenNet, ResNet18Retrain
-
-from imageio import imsave
-
+from dtoolai.utils import coerce_to_target_dim
 from dtoolai.trained import TrainedTorchModel
-
-
-def coerce_to_target_dim(im, input_format):
-    """Convert a PIL image to a fixed size and number of channels."""
-
-    mode_map = {
-        1: 'L',
-        3: 'RGB',
-        4: 'RGBA'
-    }
-
-    if im.mode not in mode_map.values():
-        raise Exception(f"Unknown image mode: {im.mode}")
-
-    ch, tdimw, tdimh = input_format
-    if ch not in mode_map:
-        raise Exception(f"Unsupported input format: {input_format}")
-
-    cdimw, cdimh = im.size
-    if (cdimw, cdimh) != (tdimw, tdimh):
-        im = im.resize((tdimw, tdimh))
-
-    if mode_map[ch] != im.mode:
-        im = im.convert(mode_map[ch])
-
-    return im
 
 
 def image_fpath_to_model_input(image_fpath, input_format):
@@ -52,10 +18,10 @@ def image_fpath_to_model_input(image_fpath, input_format):
     return as_tensor[None]
 
 
-def diagnose_input(model_input):
-    model_input_np = model_input.squeeze().cpu().numpy()
-    model_input_t = np.transpose(model_input_np, (1, 2, 0))
-    imsave('model_input_t.png', model_input_t)
+# def diagnose_input(model_input):
+#     model_input_np = model_input.squeeze().cpu().numpy()
+#     model_input_t = np.transpose(model_input_np, (1, 2, 0))
+#     imsave('model_input_t.png', model_input_t)
 
 
 @click.command()
