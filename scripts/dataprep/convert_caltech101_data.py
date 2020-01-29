760578bc6c158852925c17f8c243ffaf2c543176
Matthew Hartley <mhartley@cantab.net>
2020-01-29 20:29:23 +0000

    Update scripts

--- scripts/dataprep/convert_caltech101_data.py
+++ scripts/dataprep/convert_caltech101_data.py
@@ -5,6 +5,19 @@ from pathlib import Path
 import click
 
 
+README_TEMPLATE="""---
+dataset_name: Caltech 101 images subset
+project: dtoolAI demonstration datasets
+authors:
+  - Fei-Fei Li
+  - Marco Andreetto
+  - Marc 'Aurelio Ranzato
+reference: |
+  L. Fei-Fei, R. Fergus and P. Perona. One-Shot learning of object
+  categories. IEEE Trans. Pattern Recognition and Machine Intelligence.
+origin: http://www.vision.caltech.edu/Image_Datasets/Caltech101/
+"""
+
 @click.command()
 @click.argument('base_dirpath')
 @click.argument('staging_dirpath')
