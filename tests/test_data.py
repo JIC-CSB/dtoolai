040a9518d1ea39e2215b62f335bfe5044fcaf663
Matthew Hartley <mhartley@cantab.net>
2020-02-02 16:42:03 +0000

    Fix ImageDataSet test to use hosted resource

--- tests/test_data.py
+++ tests/test_data.py
@@ -31,11 +31,11 @@ def test_image_dataset_functional():
 
     from dtoolai.data import ImageDataSet
 
-    ids_uri = os.path.join(TEST_SAMPLE_DATA, "example_image_dataset")
+    ids_uri = "http://bit.ly/2Uho6tN"
 
     ids = ImageDataSet(ids_uri)
-    assert ids.name == "example_image_dataset"
-    assert ids.uuid == "bb27e945-b789-45d3-9b00-4808aa09ea03"
+    assert ids.name == "tiny.image.dataset.example"
+    assert ids.uuid == "839ae396-74a7-44f9-9b08-436be53b1090"
     assert len(ids) == 6
 
     assert ids.input_channels == 3
@@ -43,7 +43,7 @@ def test_image_dataset_functional():
 
     im, label = ids[0]
     assert isinstance(im, np.ndarray)
-    assert label == 2
+    assert label == 0
     
 
 def test_create_tensor_dataset_from_arrays(tmp_dir_fixture):
